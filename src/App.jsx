3a4458a13089b8be5c94cfea616bce3eff2c73bd
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2024-12-20 08:05:44 +0200

    insert testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Aos from 'aos'
-import React, { useEffect } from 'react'
+import Aos from "aos";
+import { useEffect } from "react";
 import "aos/dist/aos.css";
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Services from './components/Servics/Services';
-import Banner from './components/Banner/Banner';
-import CoffeeCup from './components/CoffeeCup/CoffeeCup';
-
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Services from "./components/Servics/Services";
+import Banner from "./components/Banner/Banner";
+import CoffeeCup from "./components/CoffeeCup/CoffeeCup";
+import Testimonials from "./components/Testimonials/Testimonials";
 const App = () => {
-  useEffect(() =>{
+  useEffect(() => {
     Aos.init({
       duration: 1000,
       once: true,
@@ -16,20 +16,18 @@ const App = () => {
       // easing: 'ease-in-out',
       // delay: 100,
       // anchorPlacement: 'top-left',
-    })
-  },[])
+    });
+  }, []);
   return (
-    <div className=''>
-      <Navbar/>
-      <Home/>
-      <Services/>
+    <div className="">
+      <Navbar />
+      <Home />
+      <Services />
       {/* <CoffeeCup/> */}
-      <Banner/>
-
+      <Banner />
+      <Testimonials />
     </div>
-   
-  
-  )
-}
+  );
+};
 
-export default App
+export default App;
