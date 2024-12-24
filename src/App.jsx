bdd90d967a50553fc6c4c52c583dd67c33271a56
Smah-amen <samahamen20001@gmail.com>
2024-12-24 21:17:03 +0200

    add routes in app and main

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import { useEffect } from "react";
 import "aos/dist/aos.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
+import HeaderPage from "./components/Home/Header";
+import Menu from "./components/Menu";
+
 const App = () => {
   useEffect(() => {
     Aos.init({
       duration: 1000,
-      // once: true,
-      // mirror: true,
-      // easing: 'ease-in-out',
-      // delay: 100,
-      // anchorPlacement: 'top-left',
     });
   }, []);
+
   return (
-    <div className="">
+    <div>
       <Navbar />
-      <Home />
+      <HeaderPage />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        {/* <Route path="*" element={<h1>404 - Page Not Found</h1>} /> */}
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default Menu
