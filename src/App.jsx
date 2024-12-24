5fadeeb99870f63c521ed314b484cd07685add96
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2024-12-24 06:49:32 +0200

    show mixes component

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       <Navbar />
       <Home />
       <Services />
-      {/* <CoffeeCup/> */}
+      <CoffeeCup />
       <Banner />
       <AppSection />
       <Testimonials />
