8cec09609f2d4337154f0961b45e3035b0c7172c
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-04 05:21:14 +0200

    update contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import ContactForm from "./ContactForm";
+import Hero from "../Hero";
+
+export default function Contact() {
+  return (
+    <div>
+      <Hero name={"Contact us"} />
+      <ContactForm />
+    </div>
+  );
+}
