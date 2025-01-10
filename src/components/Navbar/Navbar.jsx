6d0db98b6b71ff32079244660bad9c7d39d679f9
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 10:25:00 +0200

    update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,20 @@ import { FaCoffee } from "react-icons/fa";
 const Navbar = () => {
   return (
     // <div className="bg-secondary/30  text-white  shadow-md rounded-full  absolute top-1 left-1/2 transform -translate-x-1/2 w-[90%] z-10 ">
-    <div className="bg-secondary  text-white   ">
-      <div className="container py-2 ">
+    <div className="bg-secondary text-white">
+      <div className="container py-2">
         <div className="flex justify-between items-center gap-4">
           <div>
             <NavLink
               to="/"
               className="font-bold text-xl sm:text-3xl flex justify-center items-center gap-2 tracking-wider font-cursive"
             >
-              <img 
-              loading="lazy"
-              src="logo.jpg" alt="" className="w-12 rounded-full" />
+              <img
+                loading="lazy"
+                src="logo.jpg"
+                alt=""
+                className="w-12 rounded-full"
+              />
               Coffe Cafe
             </NavLink>
           </div>
@@ -70,9 +73,10 @@ const Navbar = () => {
                 </NavLink>
               </li>
             </ul>
-            <Link to="/order">
+            {/* Check on the local storage to find tokin */}
+            <Link to="/login">
               <button className="bg-primary/100 px-4 py-2 rounded-full hover:scale-105 duration-200 flex items-center gap-3">
-                order
+                Order
                 <FaCoffee className="text-xl cursor-pointer" />
               </button>
             </Link>
