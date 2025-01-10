89640cba25f3d0764d5d8ac42b62bc58896e2fdc
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 03:00:43 +0200

    update Modern Design file

--- src/components/Forms/ModernDesign.jsx
+++ src/components/Forms/ModernDesign.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
 export default function ModernDesign() {
-  return <div>Modern Design</div>;
+  return {
+    form: "modern-login mx-auto my-20 overflow-hidden bg-slate-200 p-[30px] rounded-xl pt-10 relative transform w-1/2 transition-all duration-300 before:content-[''] before:absolute before:w-[650px] before:h-[650px] before:-z-10 before:bg-[#DA9F5B] before:left-[45%] before:-bottom-[135%] before:animate-[spin_6s_infinite_linear] after:content-[''] after:absolute after:w-[650px] after:h-[650px] after:left-[35%] after:-bottom-[125%] after:animate-[spin_7s_infinite] after:bg-primary/65 after:-z-10",
+    input: "block rounded-md text-lg bg-white w-full border-0 p-[10px] my-4",
+    button:
+      "cursor-pointer text-white text-lg uppercase w-20 border-0 py-[10px] mt-[10px] rounded-[5px] bg-secondary transition-all duration-300 hover:bg-primary",
+  };
 }
