367dafc221cfee8c15659931509d840a86284dd6
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 10:28:02 +0200

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Aos from "aos";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "aos/dist/aos.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
-
+import Registration from "./components/Forms/Registeration";
 import Menu from "./components/Menu";
 import About from "./components/About/About";
 import { NotFound } from "./components/NotFound";
@@ -14,26 +14,32 @@ import Contact from "./components/Contact/Contact";
 import CoffeeDetails from "./components/CoffeeDetails";
 
 const App = () => {
+  const location = useLocation();
+  const [bold, setBold] = useState(false);
   useEffect(() => {
     Aos.init({
       duration: 1000,
     });
   }, []);
-
+  useEffect(() => {
+    location.pathname === "/login" || location.pathname === "/signup"
+      ? setBold(false)
+      : setBold(true);
+  }, [location.pathname]);
   return (
     <div className="">
-      <Navbar />
+      {bold && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-
+        <Route path="/login" element={<Registration />} />
+        <Route path="/signup" element={<Registration />} />
         <Route path="/CoffeeDetails/:id" element={<CoffeeDetails />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      {bold && <Footer />}
     </div>
   );
 };
