6b33250244604cfbbfe0e806f62ce1a4efa7be54
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 07:04:20 +0200

    create modern design

--- src/components/Forms/Design.jsx
+++ src/components/Forms/Design.jsx
@@ -1,10 +1,29 @@
 export default function Design() {
   return {
+    default: {},
     modern: {
-      form: "modern-login mx-auto my-20 overflow-hidden bg-slate-200 p-[30px] rounded-xl pt-10 relative transform w-1/2 transition-all duration-300 before:content-[''] before:absolute before:w-[650px] before:h-[650px] before:-z-10 before:bg-[#DA9F5B] before:left-[45%] before:-bottom-[135%] after:content-[''] after:absolute after:w-[650px] after:h-[650px] after:left-[35%] after:-bottom-[125%] after:animate-[spin_7s_infinite] before:animate-[spin_6s_infinite_linear] after:bg-primary/65 after:-z-10",
-      input: "block rounded-md text-lg bg-white w-full border-0 p-[10px] my-4",
-      button:
-        "cursor-pointer text-white text-lg uppercase w-fit px-5 border-0 py-[10px] mt-[10px] rounded-[5px] bg-secondary transition-all duration-300 hover:bg-primary",
+      form: "modern-login mx-auto my-20 overflow-hidden bg-slate-200 p-[30px] rounded-xl pt-7 relative transform w-full lg:w-2/3 transition-all duration-300 before:content-[''] before:absolute before:-z-10 after:bg-primary/65 before:bg-[#DA9F5B] before:w-[750px] before:h-[700px] before:left-[55%] before:-top-[10px] after:w-[750px] after:h-[700px] after:left-[45%] after:-top-[5px] after:content-[''] after:absolute after:animate-[spin_7s_infinite] before:animate-[spin_6s_infinite_linear] after:-z-10",
+      header: {
+        container: "flex items-center justify-between",
+        title: "text-4xl mb-3 capitalize",
+        link: "cursor-pointer text-lg",
+      },
+      inputField: {
+        container: "flex flex-col-reverse",
+        input:
+          "peer z-20 outline-none border-0 p-[10px] text-lg rounded-md duration-500 relative placeholder:duration-500 placeholder:absolute focus:placeholder:pt-10",
+        label:
+          "pl-2 z-10 text-lg duration-500 opacity-0 peer-focus:opacity-100 -translate-y-5 peer-focus:translate-y-0",
+      },
+      navigation: {
+        container: "flex justify-between items-center flex-wrap gap-x-4",
+        button:
+          "cursor-pointer text-white text-lg uppercase w-fit px-5 border-0 py-[10px] mt-[30px] rounded-md bg-secondary transition-all duration-300 hover:bg-primary",
+        anotherOtion: {
+          text: "text-black text-end text-pretty mt-[30px]",
+          link: "border-b-[1px] border-b-black duration-300 hover:border-b-white capitalize",
+        },
+      },
     },
   };
 }
