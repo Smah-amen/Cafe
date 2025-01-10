6a4cd40d475af75924a4485fe6bed1dd5e3e8b9d
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 03:14:34 +0200

    update ModernDesign file name

---
+++ src/components/Forms/Design.jsx
@@ -0,0 +1,10 @@
+export default function Design() {
+  return {
+    modern: {
+      form: "modern-login mx-auto my-20 overflow-hidden bg-slate-200 p-[30px] rounded-xl pt-10 relative transform w-1/2 transition-all duration-300 before:content-[''] before:absolute before:w-[650px] before:h-[650px] before:-z-10 before:bg-[#DA9F5B] before:left-[45%] before:-bottom-[135%] after:content-[''] after:absolute after:w-[650px] after:h-[650px] after:left-[35%] after:-bottom-[125%] after:animate-[spin_7s_infinite] before:animate-[spin_6s_infinite_linear] after:bg-primary/65 after:-z-10",
+      input: "block rounded-md text-lg bg-white w-full border-0 p-[10px] my-4",
+      button:
+        "cursor-pointer text-white text-lg uppercase w-fit px-5 border-0 py-[10px] mt-[10px] rounded-[5px] bg-secondary transition-all duration-300 hover:bg-primary",
+    },
+  };
+}
