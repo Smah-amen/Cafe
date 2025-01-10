2e5d3683e827f1edb98d3016c1b16a7a6f82dec8
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 10:33:05 +0200

    update Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import CardComponent from './CardComponent'
-import Hero from './Hero'
+import CardComponent from "./CardComponent";
+import Hero from "./Hero";
 
 const Menu = () => {
   return (
     <div>
-      <Hero name={"Menue"} />
-      <CardComponent/>
+      <Hero name={"Menu"} />
+      <CardComponent />
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
