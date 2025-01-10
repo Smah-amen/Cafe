68fd2e00bfa1e5a5f23bf791dab97c13a0388816
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-10 10:24:21 +0200

    update registeration component

--- src/components/Forms/Registeration.jsx
+++ src/components/Forms/Registeration.jsx
@@ -1,5 +1,54 @@
-import React from "react";
-
+import { useLocation } from "react-router";
+import Forms from "./Forms";
+import Design from "./Design";
 export default function Registeration() {
-  return <div> Registration </div>;
+  const location = useLocation().pathname.split("/")[1];
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <Forms
+          type={location}
+          design={Design().default}
+          items={
+            location === "signup"
+              ? [
+                  {
+                    label: "Name",
+                    type: "text",
+                    placeholder: "Enter your name",
+                  },
+                  {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "Enter your email",
+                  },
+                  {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Enter your password",
+                  },
+                  {
+                    label: "Confirm Password",
+                    type: "password",
+                    placeholder: "Confirm Password",
+                  },
+                ]
+              : [
+                  {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "Enter your email",
+                  },
+                  {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Enter your password",
+                  },
+                ]
+          }
+        />
+      </div>
+    </>
+  );
 }
