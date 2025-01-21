a3c281f24406fd527d6b5f3190704b67896bc1cf
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-21 08:47:56 +0200

    update services file

--- src/components/Home/Servics/Services.jsx
+++ src/components/Home/Servics/Services.jsx
@@ -11,9 +11,7 @@ const Services = () => {
     try {
       const resp = await axios.get("https://api.sampleapis.com/coffee/hot");
       setData(resp.data);
-      console.log(resp.data);
       setLoading(false);
-      
     } catch (err) {
       setData([{ title: "Error", image: "error-image.jpg" }]);
     }
@@ -23,7 +21,6 @@ const Services = () => {
     getData();
     setLoading(true);
     AOS.init();
-
   }, []);
 
   return (
@@ -45,54 +42,56 @@ const Services = () => {
         </div>
 
         {loading ? (
-        <div className=" flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit  mx-auto">
-          <img
-          loading="lazy" 
-          src="loading.jpg" alt="" className="w-full h-full" />
-        </div>
-      ) :(
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-          {data.slice(0, 6).map((coffee, index) => (
-            <div
-              key={index}
-              data-aos="fade-right"
-              data-aos-delay={index * 100}
-              className="max-w-[420px] bg-white  overflow-hidden group"
-            >
-              <div className="absolute top-2 left-2 bg-green-600 text-white text-sm font-bold px-2 py-1 rounded-full">
-                50% off - $2.50
-              </div>
-
-              <img
-                src={coffee.image}
-                alt={coffee.title}
-                className="w-full h-[350px] border-4 border-slate-100 shadow-md rounded-xl  object-cover  group-hover:scale-105 duration-300"
-              />
-
-              <div className="p-4 text-center">
-                <div className="flex justify-between items-center">
-                  <h2 className="text-xl text-primary font-bold mb-2">
-                    {coffee.title}
-                  </h2>
-                  <p className="text-primary text-lg font-bold">$5.00</p>
+          <div className=" flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit  mx-auto">
+            <img
+              loading="lazy"
+              src="loading.jpg"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+            {data.slice(0, 6).map((coffee, index) => (
+              <div
+                key={index}
+                data-aos="fade-right"
+                data-aos-delay={index * 100}
+                className="max-w-[420px] bg-white  overflow-hidden group"
+              >
+                <div className="absolute top-2 left-2 bg-green-600 text-white text-sm font-bold px-2 py-1 rounded-full">
+                  50% off - $2.50
                 </div>
-                <p className="text-gray-600 mb-4 line-clamp-3 ">
-                  {coffee.description}
-                </p>
-                <div className="flex justify-start gap-3 items-start">
-                  <button className="bg-primary text-white px-4 py-2 rounded-full">
-                    Order Now
-                  </button>
-                  <button className="bg-primary text-white px-4 py-2 rounded-full">
-                    Details
-                  </button>
+
+                <img
+                  src={coffee.image}
+                  alt={coffee.title}
+                  className="w-full h-[350px] border-4 border-slate-100 shadow-md rounded-xl  object-cover  group-hover:scale-105 duration-300"
+                />
+
+                <div className="p-4 text-center">
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-xl text-primary font-bold mb-2">
+                      {coffee.title}
+                    </h2>
+                    <p className="text-primary text-lg font-bold">$5.00</p>
+                  </div>
+                  <p className="text-gray-600 mb-4 line-clamp-3 ">
+                    {coffee.description}
+                  </p>
+                  <div className="flex justify-start gap-3 items-start">
+                    <button className="bg-primary text-white px-4 py-2 rounded-full">
+                      Order Now
+                    </button>
+                    <button className="bg-primary text-white px-4 py-2 rounded-full">
+                      Details
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-)}
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
