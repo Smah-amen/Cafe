7823cdb378461b29a4e7c2581dc06b625b55f035
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-22 07:59:16 +0200

    update animation component

--- src/components/PageTransition/Transition.jsx
+++ src/components/PageTransition/Transition.jsx
@@ -1,5 +1,21 @@
-import React from "react";
-
+// import { gsap } from "gsap";
+import "./TransitionStyle.css";
 export default function Transition() {
-  return <div>page transition</div>;
+  return (
+    <div className="fixed top-0 left-0 w-[100vw] h-[100vh] flex flex-col z-50 pointer-events-none">
+      {[
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+      ].map((item, index) => (
+        <div key={index} className={"flex flex-1 pt-row-" + index}>
+          {item.map((item, index) => (
+            <div
+              key={index}
+              className="ptr-block flex-1 bg-[#a37745] scale-y-[1] will-change-transform"
+            ></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
