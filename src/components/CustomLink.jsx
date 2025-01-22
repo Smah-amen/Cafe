88777ece9ac11de70c8391aa66aeb55673416557
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-22 07:54:40 +0200

    update custom link component

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { useLocation, useNavigate } from "react-router";
+import { animateTransition } from "./PageTransition/Animation";
+import { useState } from "react";
+export default function CustomLink({
+  to,
+  className,
+  isActive,
+  children,
+  isNotActive,
+  ...props
+}) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [curser, setCurser] = useState("pointer");
+  const Active = location.pathname === to;
 
-export default function CustomLink() {
-  return <div>Link</div>;
+  function handelClick(e) {
+    e.preventDefault();
+    if (to && !to.startsWith("#") && to !== location.pathname) {
+      setCurser("default");
+      animateTransition().then(() => {
+        navigate(to);
+        setCurser("pointer");
+      });
+    }
+  }
+  return (
+    <div
+      {...props}
+      className={
+        Active ? className + " " + isActive : className + " " + isNotActive
+      }
+      onClick={(e) => handelClick(e)}
+      style={{ cursor: curser }}
+    >
+      {children}
+    </div>
+  );
 }
