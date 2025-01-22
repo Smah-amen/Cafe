4543df5daac287456b16df0dd1f429818ae95447
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-22 08:09:24 +0200

    change menu file location

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import CardComponent from "./CardComponent";
+import Hero from "../Hero";
+
+const Menu = () => {
+  return (
+    <div>
+      <Hero name={"Menu"} />
+      <CardComponent />
+    </div>
+  );
+};
+
+export default Menu;
