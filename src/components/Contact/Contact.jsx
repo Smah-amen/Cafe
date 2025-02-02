55c842ee964eaea22f41c28169dceba74c8c2205
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-03 00:27:55 +0200

    update contact file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,11 @@
 import ContactForm from "./ContactForm";
 import Hero from "../Hero";
+import { useEffect } from "react";
 
-export default function Contact() {
+export default function Contact({ setBold, noNavBar }) {
+  useEffect(() => {
+    setBold(noNavBar);
+  });
   return (
     <div>
       <Hero name={"Contact us"} />
