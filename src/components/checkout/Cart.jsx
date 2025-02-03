5a34939d8b3786ec87b4adb0ff0cbab82868bc05
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-03 03:13:26 +0200

    update cart file

--- src/components/checkout/Cart.jsx
+++ src/components/checkout/Cart.jsx
@@ -1,45 +1,65 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { LuPlus } from "react-icons/lu";
 import { BsDash } from "react-icons/bs";
 import { IoCloseSharp } from "react-icons/io5";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
-import { useNavigate, useParams } from "react-router";
-
+// import { useNavigate, useParams } from "react-router";
 import Hero from "../Hero";
 import CustomLink from "../CustomLink";
 import Responsive from "../Home/Testimonials/Testimonials";
 import jsonData from "../Data/Data.json";
 
 const Cart = ({ setBold, noNavBar }) => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [count, setCount] = useState(1);
+  // const { id } = useParams();
+  // const navigate = useNavigate();
+  const totalPrice = jsonData
+    .filter((item) => [1, 2, 3].includes(item.id))
+    .map((item) => item.price)
+    .reduce((prev, total) => prev + total, 0);
+  const discount = 0.1;
+  const tax = 0.14;
+  const pickup = 15;
+  const [original, setOriginal] = useState(totalPrice);
+  const [saving, setSaving] = useState(totalPrice * discount);
+  const [totalTax, setTotalTax] = useState(totalPrice * tax);
+  // let totalSummary = 0;
+  const [totalSummary, setTotalSummary] = useState(0);
+  useEffect(() => {
+    setTotalSummary(original - saving + totalTax + pickup);
+  }, [original, tax, saving, totalTax]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const increment = (count, setCount, newValue) => {
+    setCount(count + 1);
+    setOriginal(original + newValue);
+    setSaving((original + newValue) * discount);
+    setTotalTax((original + newValue) * tax);
+  };
+  const decrement = (count, setCount, newValue) => {
+    count > 1 && setCount(count - 1);
+    count > 1 && setOriginal(original - newValue);
+    count > 1 && setSaving((original - newValue) * discount);
+    count > 1 && setTotalTax((original - newValue) * tax);
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = jsonData.filter((item) => item.id === parseInt(id));
+        const response = jsonData.filter((item) => [1, 2, 3].includes(item.id));
         setData(response);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching coffee data:", error);
-        setTimeout(() => navigate("/oops!"), 3000);
+        // setTimeout(() => navigate("/oops!"), 3000);
       }
     };
-
     setLoading(true);
     fetchData();
-  }, [id, navigate]);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => count > 1 && setCount(count - 1);
+  }, []);
 
   return (
     <div>
       <Hero name={"Your Order"} />
-
       <section className="bg-white py-8 md:py-16">
         <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
           <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
@@ -56,62 +76,12 @@ const Cart = ({ setBold, noNavBar }) => {
                   </div>
                 ) : data.length > 0 ? (
                   data.map((coffe, index) => (
-                    <div
+                    <CartItem
                       key={index}
-                      className="rounded-lg p-4 shadow-sm md:p-6"
-                    >
-                      <div className="flex flex-col md:flex-row md:items-center md:justify-between md:gap-6">
-                        <a href="#" className="shrink-0">
-                          <img
-                            className="h-36 w-36 object-cover rounded-full"
-                            src={coffe.image}
-                            alt="coffee"
-                          />
-                        </a>
-                        <div className="w-full md:max-w-md">
-                          <h2 className="text-xl text-primary font-bold mb-2">
-                            {coffe.title}
-                          </h2>
-                          <a
-                            href="#"
-                            className="text-base font-medium text-gray-900 hover:underline"
-                          >
-                            {coffe.description}
-                          </a>
-                          <div className="flex items-center gap-4 mt-2">
-                            <button
-                              type="button"
-                              className="text-sm font-medium text-red-600 hover:underline"
-                            >
-                              <IoCloseSharp size={20} />
-                              Remove
-                            </button>
-                          </div>
-                        </div>
-                        <div className="flex items-center">
-                          <button
-                            onClick={increment}
-                            className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200"
-                          >
-                            <LuPlus size={15} />
-                          </button>
-                          <span className="w-10 text-center text-sm font-medium text-gray-900">
-                            {count}
-                          </span>
-                          <button
-                            onClick={decrement}
-                            className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200"
-                          >
-                            <BsDash size={20} />
-                          </button>
-                        </div>
-                        <div className="text-end md:w-32">
-                          <p className="text-base font-bold text-gray-900">
-                            $1,499
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                      coffe={coffe}
+                      increment={increment}
+                      decrement={decrement}
+                    />
                   ))
                 ) : (
                   <div className="flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit mx-auto">
@@ -142,7 +112,7 @@ const Cart = ({ setBold, noNavBar }) => {
                         Original Price
                       </dt>
                       <dd className="text-base font-medium text-white">
-                        $7,592.00
+                        ${original.toFixed(2)}
                       </dd>
                     </dl>
                     <dl className="flex items-center justify-between gap-4">
@@ -150,25 +120,29 @@ const Cart = ({ setBold, noNavBar }) => {
                         Savings
                       </dt>
                       <dd className="text-base font-medium text-green-400">
-                        -$299.00
+                        -${saving.toFixed(2)}
                       </dd>
                     </dl>
                     <dl className="flex items-center justify-between gap-4">
                       <dt className="text-base font-normal text-white">
                         Store Pickup
                       </dt>
-                      <dd className="text-base font-medium text-white">$99</dd>
+                      <dd className="text-base font-medium text-white">
+                        ${pickup.toFixed(2)}
+                      </dd>
                     </dl>
                     <dl className="flex items-center justify-between gap-4">
                       <dt className="text-base font-normal text-white">Tax</dt>
-                      <dd className="text-base font-medium text-white">$799</dd>
+                      <dd className="text-base font-medium text-white">
+                        ${totalTax.toFixed(2)}
+                      </dd>
                     </dl>
                   </div>
 
                   <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2">
                     <dt className="text-base font-bold text-white">Total</dt>
                     <dd className="text-base font-bold text-white">
-                      $8,191.00
+                      ${totalSummary.toFixed(2)}
                     </dd>
                   </dl>
                 </div>
@@ -211,3 +185,60 @@ const Cart = ({ setBold, noNavBar }) => {
 };
 
 export default Cart;
+
+function CartItem({ coffe, increment, decrement }) {
+  const [count, setCount] = useState(1);
+  return (
+    <div className="rounded-lg p-4 shadow-sm md:p-6">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between md:gap-6">
+        <a href="#" className="shrink-0">
+          <img
+            className="h-36 w-36 object-cover rounded-full"
+            src={coffe.image}
+            alt="coffee"
+          />
+        </a>
+        <div className="w-full md:max-w-md">
+          <h2 className="text-xl text-primary font-bold mb-2">{coffe.title}</h2>
+          <a
+            href="#"
+            className="text-base font-medium text-gray-900 hover:underline"
+          >
+            {coffe.description}
+          </a>
+          <div className="flex items-center gap-4 mt-2">
+            <button
+              type="button"
+              className="text-sm flex gap-2 font-medium text-red-600 hover:underline"
+            >
+              <IoCloseSharp size={20} />
+              Remove
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <button
+            onClick={() => increment(count, setCount, coffe.price)}
+            className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200"
+          >
+            <LuPlus size={15} />
+          </button>
+          <span className="w-10 text-center text-sm font-medium text-gray-900">
+            {count}
+          </span>
+          <button
+            onClick={() => decrement(count, setCount, coffe.price)}
+            className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200"
+          >
+            <BsDash size={20} />
+          </button>
+        </div>
+        <div className="text-end md:w-32">
+          <p className="text-base font-bold text-gray-900">
+            ${coffe.price * count}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
