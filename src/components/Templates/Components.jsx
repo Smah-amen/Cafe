0966c39b85f2cebc2d3ffee869230e52971d7b20
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-06 01:46:11 +0200

    update components file

--- src/components/Templates/Components.jsx
+++ src/components/Templates/Components.jsx
@@ -1,16 +1,46 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import useScreenshot from "./Screenshot";
+import Vision from "../About/Vision";
+import ContactForm from "../Contact/ContactForm";
+import HeaderPage from "../Home/Header";
+import Hero from "../Hero";
+
 export default function Components() {
-  // const components = [
-  //   { noNavBar: false, path: "/", element: Home },
-  //   { noNavBar: false, path: "/menu", element: Menu },
-  //   { noNavBar: false, path: "/about", element: About },
-  //   { noNavBar: false, path: "/contact", element: Contact },
-  //   { noNavBar: false, path: "/cart", element: Cart },
-  //   { noNavBar: false, path: "/templates", element: Templates },
-  //   { noNavBar: false, path: "/CoffeeDetails/:id", element: CoffeeDetails },
-  //   { noNavBar: true, path: "/login", element: Registration },
-  //   { noNavBar: true, path: "/signup", element: Registration },
-  //   { noNavBar: true, path: "*", element: NotFound },
-  // ];
+  const { images, createScreenshot } = useScreenshot();
+
+  const [temps, setTemps] = useState([
+    { route: "/about", name: "about", content: <Vision shot={true} /> },
+    {
+      route: "/contact",
+      name: "contact",
+      content: <ContactForm shot={true} />,
+    },
+    {
+      route: "/hero1",
+      name: "hero section 1",
+      content: <HeaderPage shot={true} />,
+    },
+    {
+      route: "/hero2",
+      name: "hero section 2",
+      content: <Hero name={"templates"} shot={true} />,
+    },
+  ]);
+
+  useEffect(() => {
+    const captureScreenshots = async () => {
+      temps.map(
+        async (page) =>
+          (page.screenShot = await createScreenshot(
+            () => page.content,
+            page.name
+          ))
+      );
+    };
+    captureScreenshots();
+  }, []);
+
   return (
     <div className="container mx-auto py-16 overflow-hidden">
       <div className="">
@@ -27,15 +57,17 @@ export default function Components() {
           </h4>
         </div>
         <div className="flex justify-evenly flex-wrap gap-y-9 gap-5 my-3">
-          {[1, 2, 3, 4].map((item, index) => (
-            <div key={index} className="bg-slate-200 rounded-lg p-7 pb-4">
-              <img
-                src="/coffe1.jpg"
-                className="w-72 h-60 rounded-xl"
-                loading="lazy"
-                alt=""
-              />
-              <p className="text-center mt-4 capitalize">Hero section 1</p>
+          {temps.map((item, index) => (
+            <div key={index} className="bg-slate-100 rounded-lg p-7 pb-4">
+              <div className="w-72 h-60 rounded-xl flex justify-center items-center overflow-hidden">
+                <img
+                  src={item?.screenShot}
+                  className="w-fit h-fit"
+                  loading="lazy"
+                  alt={item.name}
+                />
+              </div>
+              <p className="text-center mt-4 capitalize">{item.name}</p>
             </div>
           ))}
         </div>
