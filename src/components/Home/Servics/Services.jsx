a897d62a467e96dad14eef14f63565eb03c33c1f
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-07 06:05:04 +0200

    update services component

--- src/components/Home/Servics/Services.jsx
+++ src/components/Home/Servics/Services.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import jsonData from "../../Data/Data.json";
@@ -11,10 +10,6 @@ const Services = () => {
 
   const getData = async () => {
     try {
-      // const resp = await axios.get("../../Data/Data.json");
-      // console.log(resp.data);
-      // console.log(datas);
-
       setData(jsonData);
       setLoading(false);
     } catch (err) {
@@ -47,7 +42,7 @@ const Services = () => {
         </div>
 
         {loading ? (
-          <div className=" flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit  mx-auto">
+          <div className="flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit mx-auto">
             <img
               loading="lazy"
               src="loading.jpg"
@@ -56,23 +51,24 @@ const Services = () => {
             />
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 gap-y-8">
             {data.slice(0, 6).map((coffee, index) => (
               <div
                 key={index}
                 data-aos="fade-right"
                 data-aos-delay={index * 100}
-                className="max-w-[420px] bg-white  overflow-hidden group"
+                className="max-w-[420px] border-4 border-slate-100 shadow-md rounded-xl bg-white overflow-hidden group"
               >
-                <div className="absolute top-2 left-2 bg-green-600 text-white text-sm font-bold px-2 py-1 rounded-full">
+                <div className="absolute top-2 left-2 duration-300 z-10 bg-green-600 text-white text-sm font-bold px-2 py-1 rounded-full">
                   50% off - $2.50
                 </div>
-
-                <img
-                  src={coffee.image}
-                  alt={coffee.title}
-                  className="w-full h-[350px] border-4 border-slate-100 shadow-md rounded-xl  object-cover  group-hover:scale-105 duration-300"
-                />
+                <div className="h-[350px] overflow-hidden">
+                  <img
+                    src={coffee.image}
+                    alt={coffee.title}
+                    className="w-full h-full object-cover group-hover:scale-105 duration-300"
+                  />
+                </div>
 
                 <div className="p-4 text-center">
                   <div className="flex justify-between items-center">
@@ -85,15 +81,15 @@ const Services = () => {
                     {coffee.description}
                   </p>
                   <div className="flex justify-start gap-3 items-start">
-                    <CustomLink to={`/cart/${coffee.id}`} > 
-                    <button className="bg-primary text-white px-4 py-2 rounded-full">
-                      Order Now
-                    </button>
+                    <CustomLink to={{ pathname: `/cart`, state: coffee.id }}>
+                      <button className="bg-primary text-white px-4 py-2 rounded-full">
+                        Order Now
+                      </button>
                     </CustomLink>
-                    <CustomLink to={ `/coffeeDetails/${coffee.id} `} > 
-                    <button className="bg-primary text-white px-4 py-2 rounded-full">
-                      Details
-                    </button>
+                    <CustomLink to={`/coffeeDetails/${coffee.id} `}>
+                      <button className="bg-primary text-white px-4 py-2 rounded-full">
+                        Details
+                      </button>
                     </CustomLink>
                   </div>
                 </div>
