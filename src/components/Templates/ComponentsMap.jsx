94918f5e326d5e2dc9715e0984b1f3f258811051
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-07 05:05:51 +0200

    create component map file

---
+++ src/components/Templates/ComponentsMap.jsx
@@ -0,0 +1,13 @@
+import Vision from "../About/Vision";
+import ContactForm from "../Contact/ContactForm";
+import Hero from "../Hero";
+import CuffeeCup from "../Home/CoffeeCup/CoffeeCup";
+import HeaderPage from "../Home/Header";
+
+export const ComponentsMap = {
+  about: Vision,
+  cup: CuffeeCup,
+  hero1: HeaderPage,
+  hero2: Hero,
+  contact: ContactForm,
+};
