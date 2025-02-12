89a277b7b1bdf653b1bc28e09f4190b3565d0405
Smah-amen <samahamen20001@gmail.com>
2025-02-12 14:38:28 +0200

    add TestimonialsCoffee component and integrate into Cart; update CoffeeCup styling

--- src/components/Home/CoffeeCup/CoffeeCup.jsx
+++ src/components/Home/CoffeeCup/CoffeeCup.jsx
@@ -29,7 +29,7 @@ const CoffeeCup = () => {
   const [selectedCoffee, setSelectedCoffee] = useState("");
 
   return (
-    <div className="flex flex-col items-center bg-gray-100 p-6 rounded-lg shadow-lg">
+    <div className="flex flex-col items-center  p-6 ">
       {/* Title */}
       <h1 className="text-2xl font-bold text-gray-800 mb-8">
         Choose Your Coffee
