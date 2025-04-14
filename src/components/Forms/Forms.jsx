3ea61721a0f5d9b9c87882dd582cfdfcfce3ea0a
Smah-amen <samahamen20001@gmail.com>
2025-04-14 22:27:28 +0200

    Revert: merge pull request #29 from Smah-amen/lastChange

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,97 +1,189 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { loginSchema, signupSchema } from "./validationSchemas.js";
+/* eslint-disable react/prop-types */
+import { Link } from "react-router";
+import "./forms.css";
 import CustomLink from "../CustomLink";
 
 export default function Forms({ type, items, design }) {
-  const schema = type === "login" ? loginSchema : signupSchema;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  // console.log(handleSubmit);
-  
-
-  // const onSubmit = (data) => {
-    
-  //   console.log("Form data:", data);
-  // };
-
   return (
-    <design.formWrap type={type}>
-      <form onSubmit={handleSubmit()}>
-        {items.map((item, index) => (
-          <div
-            key={index}
-            data-aos="fade-up"
-            data-aos-delay={500 * (index + 1.8)}
-            data-aos-duration={1100 + (index + 1)}
-            className={design?.inputField?.container}
-          >
-            <input
-              {...register(item.name)}
-              type={item.type}
-              className={design?.inputField?.input}
-              placeholder={item.placeholder}
-              style={design?.inputField?.inputStyle}
-            />
-            {errors[item.name] && (
-              <p className="text-red-400 text-sm mt-1">{errors[item.name].message}</p>
-            )}
-            {design.name !== "default" && (
-              <span className={design?.inputField?.label}>{item.label}</span>
-            )}
-          </div>
-        ))}
-        <div className={design?.navigation?.container}>
-          <button
-            data-aos="fade-up"
-            data-aos-delay="1700"
-            data-aos-duration="1000"
-            className={design?.navigation?.button}
-            type="submit"
-          >
-            {type}
-          </button>
-          <div className={design?.navigation?.anotherOtion?.text}>
-            {type === "login" ? (
-              <>
-                Don't have an account?
-                <CustomLink
-                  className={design?.navigation?.anotherOtion?.link}
-                  to="/signup"
-                >
-                  Sign up
-                </CustomLink>
-              </>
-            ) : (
-              <>
-                Already have an account?
-                <CustomLink
-                  className={design?.navigation?.anotherOtion?.link}
-                  to="/login"
-                >
-                  Login
-                </CustomLink>
-              </>
-            )}
-          </div>
-          {design.name === "default" && (
-            <div className="mt-5">
+    <>
+      {type === "login" ? (
+        <design.formWrap type={type}>
+          {items.map((item, index) => (
+            <div
+              key={index}
+              data-aos="fade-up"
+              data-aos-delay={500 * (index + 1.8)}
+              data-aos-duration={1100 + (index + 1)}
+              className={design?.inputField?.container}
+            >
+              <input
+                type={item?.type}
+                className={design?.inputField?.input}
+                placeholder={item?.placeholder}
+                style={design?.inputField?.inputStyle}
+                required="required"
+              />
+              {design.name !== "default" && (
+                <span className={design?.inputField?.label}>{item.label}</span>
+              )}
+            </div>
+          ))}
+          <div className={design?.navigation?.container}>
+            <button
+              data-aos="fade-up"
+              data-aos-delay="1700"
+              data-aos-duration="1000"
+              className={design?.navigation?.button}
+              type="submit"
+            >
+              {type}
+            </button>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="1900"
+              data-aos-duration="900"
+              className={design?.navigation?.anotherOtion?.text}
+            >
+              Don&apos;t have an account,{" "}
               <CustomLink
-                to="/"
-                className="underline underline-offset-2 duration-300 text-white hover:text-[#DA9F5B] capitalize"
+                className={design?.navigation?.anotherOtion?.link}
+                to={"/signup"}
               >
-                Back to home
+                sign up
               </CustomLink>
             </div>
-          )}
-        </div>
-      </form>
-    </design.formWrap>
+            {design.name === "default" && (
+              <div
+                data-aos="fade-up"
+                data-aos-delay="2000"
+                data-aos-duration="800"
+                className="mt-5"
+              >
+                <CustomLink
+                  to={"/"}
+                  className="underline underline-offset-2 duration-300 text-white hover:text-[#DA9F5B] capitalize"
+                >
+                  back to home
+                </CustomLink>
+              </div>
+            )}
+          </div>
+        </design.formWrap>
+      ) : (
+        <design.formWrap type={type}>
+          {items.map((item, index) => (
+            <div
+              key={index}
+              data-aos="fade-up"
+              data-aos-delay={500 * (index + 2)}
+              data-aos-duration={1300 + (index + 1)}
+              className={design?.inputField?.container}
+            >
+              <input
+                placeholder={item?.placeholder}
+                className={design?.inputField?.input}
+                type={item?.type}
+                style={design?.inputField?.inputStyle}
+              />
+              {design.name !== "default" && (
+                <span className={design?.inputField?.label}>{item.label}</span>
+              )}
+            </div>
+          ))}
+          <div
+            data-aos="fade-up"
+            data-aos-delay="2800"
+            data-aos-duration="1300"
+            className={design?.navigation?.container}
+          >
+            <button className={design?.navigation?.button}>sign up</button>
+            <div className={design?.navigation?.anotherOtion?.text}>
+              Already have an account,{" "}
+              <CustomLink
+                className={design?.navigation?.anotherOtion?.link}
+                to={"/login"}
+              >
+                login
+              </CustomLink>
+            </div>
+            {design.name === "default" && (
+              <div className="mt-5">
+                <CustomLink
+                  to={"/"}
+                  className="underline underline-offset-2 duration-300 text-white hover:text-[#DA9F5B] capitalize"
+                >
+                  back to home
+                </CustomLink>
+              </div>
+            )}
+          </div>
+        </design.formWrap>
+      )}
+    </>
   );
 }
+
+// return (
+//   <>
+//     {type === "login" ? (
+//       <form className={design.form}>
+//         <div className={design.header.container}>
+//           <div className={design.header.title}>{type}</div>
+//           <Link to={"/"}>
+//             <IoHome className={design.header.link} />
+//           </Link>
+//         </div>
+//         {items.map((item, index) => (
+//           <div key={index} className={design.inputField.container}>
+//             <input
+//               placeholder={item.placeholder}
+//               className={design.inputField.input}
+//               type={item.type}
+//             />
+//             <span className={design.inputField.label}>{item.label}</span>
+//           </div>
+//         ))}
+//         <div className={design.navigation.container}>
+//           <button className={design.navigation.button}>Login</button>
+//           <div className={design.navigation.anotherOtion.text}>
+//             Don&apos;t have an account,{" "}
+//             <Link
+//               className={design.navigation.anotherOtion.link}
+//               to={"/signup"}
+//             >
+//               Sign up
+//             </Link>
+//           </div>
+//         </div>
+//       </form>
+//     ) : (
+//       <form className={design.form}>
+//         <div className={design.header.container}>
+//           <div className={design.header.title}>create account</div>
+//           <Link to={"/"}>
+//             <IoHome className={design.header.link} />
+//           </Link>
+//         </div>
+//         {items.map((item, index) => (
+//           <div key={index} className={design.inputField.container}>
+//             <input
+//               placeholder={item.placeholder}
+//               className={design.inputField.input}
+//               type={item.type}
+//             />
+//             <span className={design.inputField.label}>{item.label}</span>
+//           </div>
+//         ))}
+//         <div className={design.navigation.container}>
+//           <button className={design.navigation.button}>sign up</button>
+//           <div className={design.navigation.anotherOtion.text}>
+//             Already have an account,{" "}
+//             <Link className={design.navigation.anotherOtion.link} to={"/login"}>
+//               login
+//             </Link>
+//           </div>
+//         </div>
+//       </form>
+//     )}
+//   </>
+// );
