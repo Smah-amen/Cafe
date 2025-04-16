2ed5b5d90c16e96e8b9dd560a579a65f16668108
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-04-16 12:19:18 +0200

    update registeration component

--- src/components/Forms/Registeration.jsx
+++ src/components/Forms/Registeration.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useLocation } from "react-router";
 import Forms from "./Forms";
 import Design from "./Design";
@@ -8,49 +9,49 @@ export default function Registeration({ setBold, noNavBar }) {
     setBold(noNavBar);
   });
 
+  const signUpArray = [
+    {
+      label: "Name",
+      type: "text",
+      placeholder: "Enter your name",
+    },
+    {
+      label: "Email",
+      type: "email",
+      placeholder: "Enter your email",
+    },
+    {
+      label: "Password",
+      type: "password",
+      placeholder: "Enter your password",
+    },
+    {
+      label: "Confirm Password",
+      type: "password",
+      placeholder: "Confirm Password",
+    },
+  ];
+
+  const logInArray = [
+    {
+      label: "Email",
+      type: "email",
+      placeholder: "Enter your email",
+    },
+    {
+      label: "Password",
+      type: "password",
+      placeholder: "Enter your password",
+    },
+  ];
+
   return (
     <div className="overflow-hidden">
       <div>
         <Forms
           type={location}
           design={Design().default}
-          items={
-            location === "signup"
-              ? [
-                  {
-                    label: "Name",
-                    type: "text",
-                    placeholder: "Enter your name",
-                  },
-                  {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "Enter your email",
-                  },
-                  {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "Enter your password",
-                  },
-                  {
-                    label: "Confirm Password",
-                    type: "password",
-                    placeholder: "Confirm Password",
-                  },
-                ]
-              : [
-                  {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "Enter your email",
-                  },
-                  {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "Enter your password",
-                  },
-                ]
-          }
+          items={location === "signup" ? signUpArray : logInArray}
         />
       </div>
     </div>
